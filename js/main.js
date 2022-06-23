9b222a18eb47d0117ef178a407e606740c74c555
pommebarque-megumi <cache2gecko@gmail.com>
2022-06-23 12:40:52 +0900

    panelPazzle

---
+++ js/main.js
@@ -0,0 +1,102 @@
+'use strict';//厳格モード
+document.addEventListener('DOMContentLoaded',()=>{
+    const size = 4;//盤面の大きさ
+    const difficulty = 500;//ゲームの複雑さ
+    const shuffleCount = size*difficulty;//シャッフルの回数
+    const table = document.querySelector("#table");
+    const msgBox = document.querySelector("#msgBox");
+    const startBt = document.querySelector("#startBt");
+    let panels;//td要素を格納する配列
+    let emptyIdx;//空白の要素(現在)のindex
+    //初期化処理
+    const init=()=>{
+        panels=[];
+        table.textContent=null;
+        msgBox.textContent=null;
+        createStage();
+    };
+    //テーブルを作成
+    const createStage=()=>{
+        for(let i=0 ; i<size ; i++){
+            const tr=document.createElement("tr");
+            for(let j=0 ; j<size ; j++){
+                const td=document.createElement("td");
+                td.posId = i*size + j;
+                td.textContent = td.posId + 1;
+                if(td.posId === size*size-1){
+                    td.textContent="";
+                    td.classList.add("empty");
+                    emptyIdx = td.posId;
+                }
+                td.onclick=click;
+                panels.push(td);
+                tr.append(td);
+            }
+            table.append(tr);
+        }
+    };
+    startBt.addEventListener('click',()=>{
+    init();
+        for(let i=0 ; i<shuffleCount ; i++){
+            const dir = Math.floor(Math.random()*4);//0上　1右　2下　3左
+            switch(dir){
+                case 0://top
+                    if(emptyIdx < size){continue;}
+                    swap(emptyIdx-size,emptyIdx);
+                    emptyIdx -= size;
+                    break;
+                case 1://right
+                    if((emptyIdx+1)%size === 0){continue;}
+                    swap(emptyIdx+1,emptyIdx);
+                    emptyIdx++;
+                    break;
+                case 2://under
+                    if(emptyIdx >= size*(size-1)){continue;}
+                    swap(emptyIdx+size,emptyIdx);
+                    emptyIdx += size;
+                    break;
+                case 3://left
+                    if(emptyIdx%size === 0){continue;}
+                    swap(emptyIdx-1,emptyIdx);
+                    emptyIdx--;
+                    break;
+            }
+        }
+        check();
+    });
+    const click =(eve)=>{
+       const pos = eve.target.posId;
+       if(pos >= size && panels[pos-size].textContent === ""){
+        swap(pos,pos-size);
+       }else if((pos+1)%size !== 0 && panels[pos+1].textContent === ""){
+        swap(pos,pos+1);
+       }else if(pos < size*(size-1) && panels[pos+size].textContent === ""){
+        swap(pos,pos+size);
+       }else if(pos%size !== 0 && panels[pos-1].textContent === ""){
+        swap(pos,pos-1);
+       }
+       check();
+    };
+    const check =()=>{
+        let okCount =0;
+        for(let i=0 ; i<panels.length ; i++){
+            if(panels[i].textContent===""){continue;}
+            if(panels[i].posId === parseInt(panels[i].textContent)-1){
+            okCount++;
+            panels[i].classList.add("ok");
+            }else{
+                panels[i].classList.remove("ok");
+            }
+        }
+        if(okCount === size*size-1){
+            msgBox.textContent = "COMPLETE!";
+        }
+    };
+    const swap=(numPos,empPos)=>{
+        panels[empPos].textContent = panels[numPos].textContent;
+        panels[empPos].classList.remove("empty");
+        panels[numPos].textContent = "";
+        panels[numPos].classList.add("empty");
+    };
+    init();
+});
